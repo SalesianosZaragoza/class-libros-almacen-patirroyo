b3910e3c4d8db4f77e97b4347592665fd2f80cc0
Alberto Saz <118882719+patirroyo@users.noreply.github.com>
2023-12-04 00:01:50 +0000

    lo de las birras

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,14 @@
-create table students(
-	id int NOT NULL AUTO_INCREMENT,
-	nombre varchar(100) not null,
-	apellido varchar(100) not null,
-	PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS BREWERY(
+   id bigint auto_increment,
+   nombre varchar(25),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS BEER(
+   id bigint auto_increment,
+   nombre varchar(50),
+   fechaLanzamiento date,
+   brewery_id INT,
+   is_old boolean,
+   FOREIGN KEY (brewery_id) REFERENCES BREWERY(id)
 );
